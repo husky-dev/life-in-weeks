e177cbfb2fed6a6c5adb81236e0c970593c19f23
Jaroslav Khorishchenko <websnipter@gmail.com>
2024-12-18 14:34:06 +0200

    Title changed

--- src/components/Page/Footer/index.tsx
+++ src/components/Page/Footer/index.tsx
@@ -9,13 +9,13 @@ export const PageFooter: FC<Props> = ({ testId, className, style }) => {
   return (
     <footer data-testid={testId} className={mc('space-y-0.5', 'text-center text-xs', className)} style={style}>
       <div>
-        <span>{'© 2023 '}</span>
+        <span>{'© 2024 '}</span>
         <a className="link" href="https://life.husky-dev.me">
-          {'Your Life in Weeks'}
+          {'Ваше життя в тижнях'}
         </a>
       </div>
       <div>
-        <span>{'Powered by '}</span>
+        <span>{'Розроблено by '}</span>
         <a className="link" href="https://husky-dev.me" target="__blank">
           {'Husky Dev'}
         </a>
